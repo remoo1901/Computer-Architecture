d18f0dd7a074a1bc11fc69021390c2a5ff5c926a
Remon Abdou <remoo1901@hotmail.com>
2020-11-08 19:51:34 -0500

    done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,7 +1,12 @@
-"""CPU functionality."""
-
 import sys
 
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MULT = 0b10100010
+PUSH = 0b01000101
+POP = 0b01000110
+SP = 7
 
 class CPU:
     """Main CPU class."""
@@ -9,50 +14,36 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.pc = 0
-        self.reg = [0] * 8
+        self.halted = False
         self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.reg[7] = 0xF4
 
-    def ram_read(self, value):
-        return self.ram[value]
-
-    def ram_write(self, value, addr):
-        self.ram[value] = addr
-
-    def load(self, program):
+    def load(self, filename):
         """Load a program into memory."""
-
         address = 0
-        with open(program) as file:
-            for line in file:
-                split_line = line.split("#")[0]
-                command = split_line.strip()
-
-                if command == '':
+        with open(filename) as fp:
+            for line in fp:
+                comment_split = line.split("#")
+                num = comment_split[0].strip()
+                if num == '': 
                     continue
-
-                instruction = int(command, 2)
-                self.ram_write(address, instruction)
-
+                val = int(num, 2)
+                self.ram_write(val, address)
                 address += 1
 
-                # print(sys.argv)
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
-                if len(sys.argv) != 2:
-                    print("Wrong number of arguments, please pass file name")
-                    sys.exit(1)
-
-    """     for instruction in program:
-            self.ram[addr] = instruction
-            address += 1 """
+    def ram_read(self, address):
+        return self.ram[address]
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
-        elif op == "MUL":
-            self.reg[reg_a] *= self.reg[reg_b]
+        #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -64,8 +55,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            # self.fl,
-            # self.ie,
+            #self.fl,
+            #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -78,35 +69,36 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
+        while not self.halted:
+            # self.trace()
+            instruction_to_execute = self.ram[self.pc]
+            operand_a = self.ram[self.pc + 1]
+            operand_b = self.ram[self.pc + 2]
+            self.execute_instruction(instruction_to_execute, operand_a, operand_b)
 
-        HLT = 0b00000001
-        LDI = 0b10000010
-        PRN = 0b01000111
-        MUL = 0b10100010
-
-        isRunning = True
-        while isRunning:
-
-            cmd = self.ram_read(self.pc)
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
-
-            if cmd == HLT:
-                isRunning = False
-                self.pc += 1
-
-            elif cmd == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-
-            elif cmd == PRN:
-                print(self.reg[operand_a])
-                self.pc += 2
-
-            elif cmd == MUL:
-                self.alu("MUL", operand_a, operand_b)
-                self.pc += 3
-
-            else:
-                print(f"bad input: {cmd}")
-                isRunning = False
+    def execute_instruction(self, instruction, operand_a, operand_b):
+        if instruction == HLT:
+            self.halted = True
+            self.pc += 1
+        elif instruction == PRN:
+            print(self.reg[operand_a])
+            self.pc += 2
+        elif instruction == LDI:
+            self.reg[operand_a] = operand_b
+            self.pc += 3
+        elif instruction == MULT:
+            self.reg[operand_a] *= self.reg[operand_b]
+            self.pc += 3
+        elif instruction == PUSH:
+            self.reg[SP] -= 1
+            valueFromRegister = self.reg[operand_a]
+            self.ram_write(valueFromRegister, self.reg[SP])
+            self.pc += 2
+        elif instruction == POP:
+            topmostValue = self.ram_read(self.reg[SP])
+            self.reg[operand_a] = topmostValue
+            self.reg[SP] += 1
+            self.pc += 2
+        else:
+            print("quit")
+            sys.exit(1)
